9998934124cd5785e8654ba8ac44f17e59619c7d
oshit-sd <oshitsd99gmaill.com>
2020-09-14 00:01:13 +0600

    add new image

--- react-js/src/components/Images.js
+++ react-js/src/components/Images.js
@@ -19,31 +19,50 @@ export default function Images() {
     // )
 
 
-    const images = [
-        "https://images.unsplash.com/photo-1583407183677-f7e236eef23b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        "https://images.unsplash.com/photo-1509291832281-6323f499fc80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
-        "https://images.unsplash.com/photo-1576087317272-798fb5cb70b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-        "https://images.unsplash.com/photo-1596127234629-9a5ae764145c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    ]
+    const [images, setimages] = useState([
+        "https://images.unsplash.com/photo-1516026672322-bc52d61a55d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        "https://images.unsplash.com/photo-1599724290412-88e9fd9ed552?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        "https://images.unsplash.com/photo-1558817298-ef90308bbf5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        "https://images.unsplash.com/photo-1599372603794-954864d73b33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    ])
+
+    // const images = [
+    //     "https://images.unsplash.com/photo-1516026672322-bc52d61a55d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    //     "https://images.unsplash.com/photo-1599724290412-88e9fd9ed552?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    //     "https://images.unsplash.com/photo-1558817298-ef90308bbf5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    //     "https://images.unsplash.com/photo-1599372603794-954864d73b33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    // ]
 
     function ShowImages() {
         return images.map(image => {
             return (
-                <div>
-                    <img src={image}></img>
+                <div className="w-1/3">
+                    <img src={image} width="150"></img>
                 </div>
             );
         })
     }
 
+    function handleAdd() {
+        setimages([
+            ...images,
+            "https://images.unsplash.com/photo-1599358093791-b884b4890ffd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+        ])
+    }
+
     return (
         <section>
-            <div className="flex justify-center">
+            <div className="flex flex-wrap justify-center">
                 <ShowImages />
 
                 {/* <div>
                     <img src={images[0]} width="150"></img>
                 </div> */}
+            </div>
+
+            <div className="flex justify-between my-5">
+                <input type="text" className="p-2 border border-gray-800 shadow rounded" />
+                <button onClick={handleAdd} className="p-2 bg-green-600 text-white">Add New</button>
             </div>
         </section>
     )
